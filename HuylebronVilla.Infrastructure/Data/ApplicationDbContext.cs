0254549129d1cc3b31da074ceb9bd7592f6d7b76
huylebron <doquochuy27102003@gmail.com>
2024-12-23 21:59:24 +0700

    settup project + add dbContext+migration db + create villa model

---
+++ HuylebronVilla.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using HuylebronVilla.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HuylebronVilla.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Villa> Villas { get; set; }
+    }
+}
---
+++ HuylebronVilla.Infrastructure/Migrations/20241223144052_HuylebronVilla.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HuylebronVilla.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class HuylebronVilla : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
