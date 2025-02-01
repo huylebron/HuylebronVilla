718d9674e0bc2fa32a76e7ec2bdd3b1e61298f35
huylebron <doquochuy27102003@gmail.com>
2025-02-02 00:01:07 +0700

    add error notice

--- HuylebronVilla.Web/Controllers/HomeController.cs
+++ HuylebronVilla.Web/Controllers/HomeController.cs
@@ -1,6 +1,4 @@
-using HuylebronVilla.Web.Models;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
 
 namespace HuylebronVilla.Web.Controllers
 {
@@ -26,7 +24,7 @@ namespace HuylebronVilla.Web.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View();
         }
     }
 }
